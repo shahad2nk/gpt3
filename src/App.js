31ed0577ced651e0755141f6426b395536359a49
Shahad2nk <shahad2nk@gmail.com>
2024-03-03 13:56:11 +0300

    Upload Porject

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import {
+  Header,
+  Footer,
+  Blog,
+  Possibility,
+  WhatGPT3,
+  Features,
+} from "./containers";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './brand.css';
+import {google,slack, atlassian, dropbox, shopify} from './import'
+
+const Brand = () => {
+  return (
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="" /></div>
+        <div><img src={slack} alt="" /></div>
+        <div><img src={atlassian} alt="" /></div>
+       <div><img src={dropbox} alt="" /></div> 
+       <div><img src={shopify} alt="" /></div> 
+      
+      
+    </div>
+  )
+}
+
+export default Brand
